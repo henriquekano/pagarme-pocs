ffb9779e84917092937e948fd15410eca66942b3
Henrique Kano <henriquekano@hotmail.com>
2017-02-21 19:33:24 -0300

    Checkout + Transaction Capture. ASP MVC

---
+++ PagarmeAsp2/PagarmeAsp2/App_Start/WebApiConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+
+namespace PagarmeAsp2
+{
+	public static class WebApiConfig
+	{
+		public static void Register(HttpConfiguration config)
+		{
+			// Web API configuration and services
+			PagarMe.PagarMeService.DefaultApiKey = "ak_test_zXjKL8u5uxn25HNxHviPbhthNV0nL7";
+			// Web API routes
+			config.MapHttpAttributeRoutes();
+
+			config.Routes.MapHttpRoute(
+				name: "DefaultApi",
+				routeTemplate: "api/{controller}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			);
+		}
+	}
+}
---
+++ PagarmeAsp2/PagarmeAsp2/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+using PagarMe;
+
+namespace PagarmeAsp2.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			var mvcName = typeof(Controller).Assembly.GetName();
+			var isMono = Type.GetType("Mono.Runtime") != null;
+
+			ViewData["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
+			ViewData["Runtime"] = isMono ? "Mono" : ".NET";
+
+			return View();
+		}
+
+		public ActionResult Capture()
+		{
+			var amount = 1000;
+			var token = Request.Form.Get("token");
+			Transaction tx = PagarMeService.GetDefaultService().Transactions.Find(token);
+			try
+			{
+				tx.Capture(amount);
+				ViewData["Message"] = "Success";
+			}
+			catch (Exception e)
+			{
+				var x = e.StackTrace;
+				ViewData["Message"] = x;
+			}
+
+			return View();
+		}
+	}
+}
