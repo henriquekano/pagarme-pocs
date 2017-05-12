a51ef7e1007ed53de05d84cd18dabfb7d18117d6
henriquekano <henriquekano@hotmail.com>
2017-05-12 20:15:24 -0300

    Removing configs

--- upload/admin/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	// DB
-	define('DB_DRIVER', 'mysql');
-	define('DB_HOSTNAME', 'sql10.freemysqlhosting.net');
-	define('DB_USERNAME', 'sql10174125');
-	define('DB_PASSWORD', 'V8PKb816iz');
-	define('DB_DATABASE', 'sql10174125');
-	define('DB_PREFIX', 'oc_');
