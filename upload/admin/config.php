47f5dedd4b569af075076786075e3a0964ca5c2b
henriquekano <henriquekano@hotmail.com>
2017-05-12 21:11:45 -0300

    Pre installed stuff

---
+++ upload/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8082/admin/');
+define('HTTP_CATALOG', 'http://localhost:8082/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8082/admin/');
+define('HTTPS_CATALOG', 'http://localhost:8082/');
+
+// DIR
+define('DIR_APPLICATION', '/home/henrique/heroku/opencart/2.2.0.0-compiled/upload/admin/');
+define('DIR_SYSTEM', '/home/henrique/heroku/opencart/2.2.0.0-compiled/upload/system/');
+define('DIR_IMAGE', '/home/henrique/heroku/opencart/2.2.0.0-compiled/upload/image/');
+define('DIR_LANGUAGE', '/home/henrique/heroku/opencart/2.2.0.0-compiled/upload/admin/language/');
+define('DIR_TEMPLATE', '/home/henrique/heroku/opencart/2.2.0.0-compiled/upload/admin/view/template/');
+define('DIR_CONFIG', '/home/henrique/heroku/opencart/2.2.0.0-compiled/upload/system/config/');
+define('DIR_CACHE', '/home/henrique/heroku/opencart/2.2.0.0-compiled/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/henrique/heroku/opencart/2.2.0.0-compiled/upload/system/storage/download/');
+define('DIR_LOGS', '/home/henrique/heroku/opencart/2.2.0.0-compiled/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/henrique/heroku/opencart/2.2.0.0-compiled/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/home/henrique/heroku/opencart/2.2.0.0-compiled/upload/system/storage/upload/');
+define('DIR_CATALOG', '/home/henrique/heroku/opencart/2.2.0.0-compiled/upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'sql10.freemysqlhosting.net');
+define('DB_USERNAME', 'sql10174125');
+define('DB_PASSWORD', 'V8PKb816iz');
+define('DB_DATABASE', 'sql10174125');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ upload/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8082/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8082/');
+
+// DIR
+define('DIR_APPLICATION', '/home/henrique/heroku/opencart/2.2.0.0-compiled/upload/catalog/');
+define('DIR_SYSTEM', '/home/henrique/heroku/opencart/2.2.0.0-compiled/upload/system/');
+define('DIR_IMAGE', '/home/henrique/heroku/opencart/2.2.0.0-compiled/upload/image/');
+define('DIR_LANGUAGE', '/home/henrique/heroku/opencart/2.2.0.0-compiled/upload/catalog/language/');
+define('DIR_TEMPLATE', '/home/henrique/heroku/opencart/2.2.0.0-compiled/upload/catalog/view/theme/');
+define('DIR_CONFIG', '/home/henrique/heroku/opencart/2.2.0.0-compiled/upload/system/config/');
+define('DIR_CACHE', '/home/henrique/heroku/opencart/2.2.0.0-compiled/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/henrique/heroku/opencart/2.2.0.0-compiled/upload/system/storage/download/');
+define('DIR_LOGS', '/home/henrique/heroku/opencart/2.2.0.0-compiled/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/henrique/heroku/opencart/2.2.0.0-compiled/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/home/henrique/heroku/opencart/2.2.0.0-compiled/upload/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'sql10.freemysqlhosting.net');
+define('DB_USERNAME', 'sql10174125');
+define('DB_PASSWORD', 'V8PKb816iz');
+define('DB_DATABASE', 'sql10174125');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
